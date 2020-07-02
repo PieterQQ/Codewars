d1bc8c976ffbd527c5eef7e87ed3f274da9f9c94
PieterQQ <pieter444@wp.pl>
2020-07-02 09:53:52 +0200

    new solutions

---
+++ Codewars Solutions/7 kata/Complementary DNA.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        return string.Concat(dna.Select(GetComplement));
+    }
+
+    public static char GetComplement(char symbol)
+    {
+        switch (symbol)
+        {
+            case 'A':
+                return 'T';
+            case 'T':
+                return 'A';
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            default:
+                throw new ArgumentException();
+        }
+    }
+}
